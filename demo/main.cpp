b37a8ad6acee821e9212c444d13f741a2b807fb6
Overpeek <overpeek.fin@gmail.com>
2019-12-24 04:49:49 +0200

    Fully working with CMake (and VS2019 atleast)

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,21 +1,25 @@
-#include "../overpeek-engine/engine.h"
-#include "../overpeek-engine/internal_libs.h"
-#include "../overpeek-engine/graphics/textureManager.h"
-#include "../overpeek-engine/graphics/assets/singleTextureShader.h"
+#include <engine.h>
+#include <internal_libs.h>
+#include <graphics/textureManager.h>
+#include <graphics/assets/singleTextureShader.h>
 
 #include <Windows.h>
 #include <string>
 
+#if GLEW_STATIC
+void sm();
+#else
+void sm();
+#endif
 
-
-oe::SingleTextureShader* shader;
-oe::Renderer* renderer_0;
-oe::Renderer* renderer_1;
+oe::graphics::SingleTextureShader* shader;
+oe::graphics::Renderer* renderer_0;
+oe::graphics::Renderer* renderer_1;
 
 float t = 0;
 void render(float update_fraction) {
-	oe::Window::clear();
-	t += oe::GameLoop::getFrameUpdateScale();
+	oe::graphics::Window::clear();
+	t += oe::utils::GameLoop::getFrameUpdateScale();
 
 	// quads
 	/* scramble test */
@@ -26,55 +30,55 @@ void render(float update_fraction) {
 	renderer_1->clear();
 	
 	// renderer 0 submits
-	renderer_0->submit(glm::vec2(-0.5f,  0.5f), glm::vec2(0.4f), 0, glm::vec4(0.0f, 0.0f, 1.0f, 1.0f), oe::alignment::center_center, t);
-	renderer_0->submit(glm::vec2( 0.5f,  0.5f), glm::vec2(0.4f), 0, glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), oe::alignment::center_center, sin(t));
+	renderer_0->submit(glm::vec2(-0.5f,  0.5f), glm::vec2(0.4f), 0, glm::vec4(0.0f, 0.0f, 1.0f, 1.0f), oe::graphics::alignment::center_center, t);
+	renderer_0->submit(glm::vec2( 0.5f,  0.5f), glm::vec2(0.4f), 0, glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), oe::graphics::alignment::center_center, sin(t));
 
 	// renderer 1 submits
-	renderer_1->submit(glm::vec2(0.5f, -0.5f), glm::vec2(0.4f), 0, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f), oe::alignment::center_center, tan(t));
-	renderer_1->submit(glm::vec2(-0.5f, -0.5f), glm::vec2(0.4f), 0, glm::vec4(0.0f, 0.5f, 1.0f, 1.0f), oe::alignment::center_center, round(t));
-	renderer_1->submit(glm::vec2(0.0f, 0.0f), glm::vec2(0.4f), 0, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), oe::alignment::center_center, t * 100.0f);
+	renderer_1->submit(glm::vec2(0.5f, -0.5f), glm::vec2(0.4f), 0, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f), oe::graphics::alignment::center_center, tan(t));
+	renderer_1->submit(glm::vec2(-0.5f, -0.5f), glm::vec2(0.4f), 0, glm::vec4(0.0f, 0.5f, 1.0f, 1.0f), oe::graphics::alignment::center_center, round(t));
+	renderer_1->submit(glm::vec2(0.0f, 0.0f), glm::vec2(0.4f), 0, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), oe::graphics::alignment::center_center, t * 100.0f);
 
 	renderer_0->end();
 	renderer_0->render();
 	renderer_1->end();
 	//renderer_1->render();
 	
-	oe::Window::update();
-	oe::Window::input();
-	if (oe::Window::shouldClose()) oe::GameLoop::stop();
+	oe::graphics::Window::update();
+	oe::graphics::Window::input();
+	if (oe::graphics::Window::shouldClose()) oe::utils::GameLoop::stop();
 }
 
 int n = 0;
 void update() {
-	if (n++%100==99) spdlog::info("FPS: " + std::to_string(oe::GameLoop::getFPS()));
+	if (n++%100==99) spdlog::info("FPS: " + std::to_string(oe::utils::GameLoop::getFPS()));
 }
 
 int main() {
 	oe::init();
 
-	oe::Window::init(900, 600, "overpeek-engine", WINDOW_GL_DEBUG | WINDOW_MULTISAMPLE_X2 | WINDOW_TRANSPARENT);
-	oe::Window::setClearColor(0.0f, 0.0f, 0.0f, 0.2f);
-	oe::GL::setPolygonMode(0);
-	renderer_0 = new oe::Renderer(oe::types::dynamicrender, oe::types::staticrender, 5, nullptr);
-	renderer_1 = new oe::Renderer(oe::types::dynamicrender, oe::types::staticrender, 5, nullptr);
-	shader = new oe::SingleTextureShader();
+	oe::graphics::Window::init(900, 600, "overpeek-engine", WINDOW_GL_DEBUG | WINDOW_MULTISAMPLE_X2 | WINDOW_TRANSPARENT);
+	oe::graphics::Window::setClearColor(0.0f, 0.0f, 0.0f, 0.2f);
+	oe::graphics::GL::setPolygonMode(0);
+	renderer_0 = new oe::graphics::Renderer(oe::graphics::types::dynamicrender, oe::graphics::types::staticrender, 5, nullptr);
+	renderer_1 = new oe::graphics::Renderer(oe::graphics::types::dynamicrender, oe::graphics::types::staticrender, 5, nullptr);
+	shader = new oe::graphics::SingleTextureShader();
 
 	// pr matrix
-	glm::mat4 pr = glm::ortho(-oe::Window::getAspect(), oe::Window::getAspect(), 1.0f, -1.0f);
+	glm::mat4 pr = glm::ortho(-oe::graphics::Window::getAspect(), oe::graphics::Window::getAspect(), 1.0f, -1.0f);
 	shader->projectionMatrix(pr);
 	shader->useTexture(false);
 
 	//oe::TextureManager::loadTextureAtlas("res/texture/atlas.png", 0, 16);
-	oe::GL::setBackFaceCulling(false);
-	oe::GL::setSwapInterval(0);
-	oe::FrameBuffer::unbind();
-	oe::Window::viewport();
+	oe::graphics::GL::setBackFaceCulling(false);
+	oe::graphics::GL::setSwapInterval(0);
+	oe::graphics::FrameBuffer::unbind();
+	oe::graphics::Window::viewport();
 	
 	// start
-	oe::GameLoop::init(render, update, 100);
+	oe::utils::GameLoop::init(render, update, 100);
 
 	// closing
-	oe::Window::close();
+	oe::graphics::Window::close();
 	delete shader;
 	delete renderer_0;
 	delete renderer_1;
